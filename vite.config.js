881786f78d5052928c37f43034c2d9740492ea1c
KLmk1 <klimakarov@gmail.com>
2024-12-24 22:11:48 +0400

    ewker

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sitek/', // Замените "mysite" на имя вашего репозитория
 })
